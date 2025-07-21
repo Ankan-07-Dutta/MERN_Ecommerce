dc154325bf0ef08a07e0f28a02947e3c8ea2b5fa
Ankan-07-Dutta <bhaskardutta335@gmail.com>
2025-07-21 08:51:44 +0530

    craeted all mistakenly deleted files

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import app from './app.js';
+import dotenv from 'dotenv';
+dotenv.config({path:'backend/config/config.env'});
+const port = process.env.PORT || 3000;
+
+
+
+
+app.listen(port, ()=> {
+    console.log(`server is running on port ${port}`);    
+})
