7234c4821934ac7f4247684ce40bad4d49458536
Ankan-07-Dutta <bhaskardutta335@gmail.com>
2025-07-21 12:06:51 +0530

    created update and delete function

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -22,7 +22,11 @@ export const getAllProducts = async (req,res) => {
 // Update Product
 
 export const updateProduct = async(req,res)=> {
-    let product = await Product.findById(req.params.id);
+
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new:true,
+        runValidators:true
+    });
     if(!product){
         return res.status(500).json({
             success:false,
@@ -30,15 +34,40 @@ export const updateProduct = async(req,res)=> {
         })
     }
 
-    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-        new:true,
-        runValidators:true
-    });
-
     res.status(200).json({
         success:true,
         product
     })
-    
-    
+       
+}
+
+// Delete product
+
+export const deleteProduct = async(req, res)=>{    
+    const product = await Product.findByIdAndDelete(req.params.id);
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message: "Product not found"
+        })
+    }
+    res.status(200).json({
+        success:true,
+        message:"Product deleted succesfully"
+    })
+}
+
+// Accessing Single Product
+export const getSingleProduct = async(req, res)=>{
+    const product = await Product.findById(req.params.id);
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message: "Product not found"
+        })
+    }
+    res.status(200).json({
+        success:true,
+        product
+    })
 }
