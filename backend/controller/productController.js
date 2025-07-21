c8478486aabb16058cf6665642b5ccc894b30a52
Ankan-07-Dutta <bhaskardutta335@gmail.com>
2025-07-21 08:43:06 +0530

    few changes

--- backend/controller/productController.js
+++
@@ -1,5 +0,0 @@
-export const getAllProducts = (req,res) => {
-    res.status(200).json({
-        message: "All Products"
-    })
-}
--- backend/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getAllProducts } from '../controller/productController.js';
-const router = express.Router();
-
-
-// Routes
-router.route("/products").get(getAllProducts); 
-
-export default router;
