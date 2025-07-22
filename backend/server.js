1f70d0aa9df4144861ce9d25c2db31ec9d1f1d59
Ankan-07-Dutta <bhaskardutta335@gmail.com>
2025-07-22 08:48:31 +0530

    Included error functions

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,31 @@ import dotenv from 'dotenv';
 import { connectMongoDatabase } from './config/db.js';
 dotenv.config({path:'backend/config/config.env'});
 connectMongoDatabase();
+//Handle uncaught exception error
+process.on('uncaughtException',(err)=>{
+    console.log(`Error: ${err.message}`);
+    console.log(`Server is shutting down, due to uncaught exception errors`);
+    process.exit(1);
+    
+})
+
 const port = process.env.PORT || 3000;
 
 
 
 
-app.listen(port, ()=> {
+const server = app.listen(port, ()=> {
     console.log(`server is running on port ${port}`);    
 });
+
+
+
+
+process.on('unhandledRejection',(err)=>{
+    console.log(`Error: ${err.message}`);
+    console.log(`Server is shutting down, due to unhandled promise rejection`);
+    server.close(()=>{
+        process.exit(1);
+    })
+      
+})
