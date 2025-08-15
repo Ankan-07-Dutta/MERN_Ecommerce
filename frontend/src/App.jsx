975e37ba88625c131f2ebe3e033fce33266f9453
Ankan-07-Dutta <bhaskardutta335@gmail.com>
2025-08-15 11:09:57 +0530

    Checkoupath process payment page done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,6 +17,7 @@ import ResetPassword from './User/ResetPassword';
 import Cart from './Cart/Cart';
 import Shipping from './Cart/Shipping';
 import OrderConfirm from './Cart/OrderConfirm';
+import Payment from './Cart/Payment';
 
 const App = () => {
   const {isAuthenticated, user} = useSelector(state => state.user);
@@ -47,6 +48,7 @@ const App = () => {
         <Route path='/cart' element={<Cart />}/>
         <Route path='/shipping' element={<ProtectedRoute element={<Shipping />} />} />
         <Route path='/order/confirm' element={<ProtectedRoute element={<OrderConfirm />} />} />
+        <Route path='/process/payment' element={<ProtectedRoute element={<Payment />} />} />
 
 
       </Routes>
